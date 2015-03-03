274812591a6117cc564fdc6436aab4ea8a7384b4
jenkins <c.spiliotopoulos@internetq.com>
2015-03-03 14:08:59 +0200

    Fixed branch messup

---
+++ chrome-ext/background.js
@@ -0,0 +1,210 @@
+var globals = {
+
+	tabs : {
+		selected : null
+	},
+
+	// the loaded Swagger API definition
+	api : null,
+	
+	// the loaded API catalog definition
+	catalog: null,
+	
+	type: null,
+	
+	// the page URL
+	url : null
+};
+
+
+/**
+ * Called when a different tab has been selected
+ */
+chrome.tabs.onSelectionChanged.addListener(function(tabId, info) {
+
+	// remember the selected tab
+	globals.tabs.selected = tabId;
+
+});
+
+
+/**
+ * Called when the user clicks on the page action
+ */
+chrome.pageAction.onClicked.addListener(function(tab) {
+
+	// create a new window that will load the visualizer
+	chrome.windows.create({url: chrome.extension.getURL("canvas.html"), type: "popup"}, function() {
+	
+		// send a message to the new window with the API definition
+		var msg = null;
+		
+		if (globals.type === 'api')
+			msg = { name: 'onVisualizeApi', api: globals.api };
+		else if (globals.type === 'catalog')
+			msg = { name: 'onVisualizeCatalog', catalog: globals.catalog };
+			
+		chrome.runtime.sendMessage( undefined, msg, undefined );
+		
+	});
+});
+
+
+/**
+ * Loads a linked API catalog
+ * from the given resource URL.
+ */
+function loadLinkedApiCatalog(url, cb) {
+	
+	try {
+		
+		// GET the resource
+		$.ajax({
+			url : url,
+			success : function(content, status) {
+				
+				// return the content fetched
+				cb(null, content);
+			},
+			error : function(jqXHR, err) {
+				console.error(jqXHR.statusText);
+				cb("Resource at [" + url + "] could not be fetched.");
+			}
+		});
+		
+	} catch (e) {
+		console.error(e);
+	}
+	
+}
+
+/**
+ * Loads a linked API catalog
+ * from the given resource URL.
+ */
+function onLinkedCatalogFetched(err, catalog) {
+	
+	try {
+		
+		var msg = {
+				name : 'onCatalogFetched',
+				catalog : catalog
+			};
+			chrome.runtime.sendMessage(undefined, msg, undefined);
+		
+	} catch (e) {
+		console.error(e);
+	}
+	
+}
+
+/**
+ * Loads a linked API catalog
+ * from the given resource URL.
+ */
+function loadSwaggerDefinition(url, cb) {
+	
+	try {
+		
+		// GET the resource
+		$.ajax({
+			url : url,
+			success : function(content, status) {
+				
+				// return the content fetched
+				cb(null, content);
+			},
+			error : function(jqXHR, err) {
+				console.error(jqXHR.statusText);
+				cb("Resource at [" + url + "] could not be fetched.");
+			}
+		});
+		
+	} catch (e) {
+		console.error(e);
+	}
+	
+}
+
+/**
+ * Loads a linked API catalog
+ * from the given resource URL.
+ */
+function onSwaggerDefinitionFetched(err, api) {
+	
+	try {
+		
+		/*
+		 * 
+		 */
+		
+		
+		var msg = {
+				name : 'onVisualizeApi',
+				api : api
+			};
+			chrome.runtime.sendMessage(undefined, msg, undefined);
+		
+	} catch (e) {
+		console.error(e);
+	}
+	
+}
+
+
+
+/**
+ * Bind global event listener
+ */
+chrome.runtime.onMessage.addListener(function(msg, sender, cb) {
+
+	// call the callback
+	switch (msg.name) {
+	case 'onSwaggerDefinition':
+		
+		// store the API definition
+		globals.type = 'api';
+		globals.api = msg.api;
+
+		// enable the page action
+		chrome.pageAction.setIcon({path: 'res/swagger-32.png', tabId: sender.tab.id});
+		chrome.pageAction.show(sender.tab.id);
+		break;
+		
+	case 'onApiCatalogDefinition':	
+		
+		// store the API catalog definition
+		globals.type = 'catalog';
+		globals.catalog = msg.catalog;	
+		
+		// enable the page action
+		chrome.pageAction.setIcon({path: 'res/catalog-32.png', tabId: sender.tab.id});
+		chrome.pageAction.show(sender.tab.id);
+		break;
+	
+	case 'onLoadLinkedCatalog':
+		
+		// load the linked API catalog
+		loadLinkedApiCatalog( msg.url, onLinkedCatalogFetched );
+		break;
+	
+	case 'onLoadSwaggerDefinition':
+		
+		// load the linked API catalog
+		loadSwaggerDefinition( msg.url, onSwaggerDefinitionFetched );
+		break;	
+		
+	default:
+
+		// reset vars
+		globals.type = null;
+		globals.api = null;
+		globals.catalog = null;
+		
+		// disable the page action
+		chrome.pageAction.hide(sender.tab.id);
+
+		break;
+	}
+
+});
---
+++ chrome-ext/content-script.js
@@ -0,0 +1,164 @@
+/**
+ * Grabs the page content, detect if it contains a Swagger API definition and
+ * tries to parse it.
+ */
+var parseSwaggerDefinition = function(url) {
+	
+	// default message - invalid content
+	var msg = {
+		name : 'onInvalidContent'
+	};
+	
+	try {
+		
+		/*
+		 * try to parse the Swagger definition
+		 */
+		swagger.ed.SwaggerParser.parse(url, function(err, api) {
+			
+			if (err)
+				return chrome.runtime.sendMessage(undefined, msg, undefined);
+			
+			msg = {
+				name : 'onSwaggerDefinition',
+				api : api
+			};
+		
+			chrome.runtime.sendMessage(undefined, msg, undefined);
+			
+		});
+		
+	} catch (e) {
+		
+		chrome.runtime.sendMessage(undefined, msg, undefined);
+	}
+	
+	return null;
+}
+
+/**
+ * Checks whether the page contains Swagger content.
+ * 
+ * @param content
+ *          The page content
+ */
+function checkIfSwaggerContent(content) {
+	
+	try {
+		
+		/*
+		 * Is this a valid YAML or JSON document?
+		 */
+		var json = jsyaml.load(content);
+		
+		/*
+		 * Does it have a 'swagger' or 'swaggerVersion' attribute
+		 */
+
+		// check for the 'swaggerVersion' attribute
+		if ((typeof json.swaggerVersion == 'undefined')
+				&& (typeof json.swagger == 'undefined'))
+			throw "Not a Swagger document";
+		
+		// return the Swagger document
+		return json;
+		
+	} catch (e) {
+		
+	}
+	
+	return null;
+}
+
+/**
+ * Checks whether the page contains APIs catalog content.
+ * 
+ * @param content
+ *          The page content
+ */
+function checkIfApisCatalogContent(content) {
+	
+	try {
+		
+		/*
+		 * Is this a valid YAML or JSON document?
+		 */
+		var json = jsyaml.load(content);
+		
+		/*
+		 * Does it have a 'swagger' or 'swaggerVersion' attribute
+		 */
+
+		// check for the 'swaggerVersion' attribute
+		if ((typeof json.SpecificationVersion == 'undefined')
+				&& (typeof json.apis == 'undefined'))
+			throw "Not an APIs catalog  document";
+		
+		// return the catalog document
+		return json;
+		
+	} catch (e) {
+		
+	}
+	
+	return null;
+}
+
+
+/**
+ * Scans the page and tries to detect if it contains a compatible content type.
+ */
+function detectContentType() {
+	
+	try {
+		
+		// get the document body
+		var body = document.body;
+		
+		// get the outer text of the page body
+		var content = body.outerText;
+		
+		/*
+		 * First check for Swagger content
+		 */
+		var data = checkIfSwaggerContent(content);
+		
+		/*
+		 * If the page contains a Swagger API document, try to parse it
+		 */
+		if (data)
+			return parseSwaggerDefinition(document.URL);
+		
+		/*
+		 * Next check for APIs.json content
+		 */
+		data = checkIfApisCatalogContent(content);
+		
+		if (data) {
+			
+			var msg = {
+				name : 'onApiCatalogDefinition',
+				catalog : data
+			};
+			
+			/*
+			 * broadcast the message
+			 */
+			chrome.runtime.sendMessage(undefined, msg, undefined);
+		}
+		
+	} catch (e) {
+		console.error(e);
+	}
+	
+}
+
+// The background page is asking us to find an address on the page.
+if (window == top) {
+	
+	/*
+	 * detect the content type of the page
+	 */
+	detectContentType();
+	
+}
