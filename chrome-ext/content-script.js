07a6798a779a5a820ec4e550318346c53bbab4db
c.spiliotopoulos <c.spiliotopoulos@akazoo.com>
2017-03-29 20:38:44 +0300

    Important optimizations
    - Parses Swagger model directly from the downloaded JSON / YAML content,
      instead from loading the resource from the URL twice
    - Now local documents can be parsed using the file:// protocol

--- chrome-ext/content-script.js
+++ chrome-ext/content-script.js
@@ -2,163 +2,162 @@
  * Grabs the page content, detect if it contains a Swagger API definition and
  * tries to parse it.
  */
-var parseSwaggerDefinition = function(url) {
-	
+var parseSwaggerDefinition = function (url, content) {
+
 	// default message - invalid content
 	var msg = {
-		name : 'onInvalidContent'
+		name: 'onInvalidContent'
 	};
-	
+
 	try {
-		
+
 		/*
 		 * try to parse the Swagger definition
 		 */
-		swagger.ed.SwaggerParser.parse(url, function(err, api) {
-			
+		swagger.ed.SwaggerParser.parse(url, content, function (err, api) {
 			if (err)
 				return chrome.runtime.sendMessage(undefined, msg, undefined);
-			
+
 			msg = {
-				name : 'onSwaggerDefinition',
-				api : api
+				name: 'onSwaggerDefinition',
+				api: api
 			};
-		
+
 			chrome.runtime.sendMessage(undefined, msg, undefined);
-			
+
 		});
-		
+
 	} catch (e) {
-		
 		chrome.runtime.sendMessage(undefined, msg, undefined);
 	}
-	
+
 	return null;
-}
+};
 
 /**
  * Checks whether the page contains Swagger content.
- * 
+ *
  * @param content
  *          The page content
  */
-function checkIfSwaggerContent(content) {
-	
+function checkIfSwaggerContent (content) {
 	try {
-		
+
 		/*
 		 * Is this a valid YAML or JSON document?
 		 */
 		var json = jsyaml.load(content);
-		
+
 		/*
 		 * Does it have a 'swagger' or 'swaggerVersion' attribute
 		 */
 
 		// check for the 'swaggerVersion' attribute
 		if ((typeof json.swaggerVersion == 'undefined')
-				&& (typeof json.swagger == 'undefined'))
-			throw "Not a Swagger document";
-		
+			&& (typeof json.swagger == 'undefined'))
+			throw 'Not a Swagger document';
+
 		// return the Swagger document
 		return json;
-		
-	} catch (e) {
-		
-	}
-	
+
+	} catch (e) {}
+
 	return null;
 }
 
 /**
  * Checks whether the page contains APIs catalog content.
- * 
+ *
  * @param content
  *          The page content
  */
-function checkIfApisCatalogContent(content) {
-	
+function checkIfApisCatalogContent (content) {
 	try {
-		
+
 		/*
 		 * Is this a valid YAML or JSON document?
 		 */
 		var json = jsyaml.load(content);
-		
+
 		/*
 		 * Does it have a 'swagger' or 'swaggerVersion' attribute
 		 */
 
 		// check for the 'swaggerVersion' attribute
 		if ((typeof json.SpecificationVersion == 'undefined')
-				&& (typeof json.apis == 'undefined'))
-			throw "Not an APIs catalog  document";
-		
+			&& (typeof json.apis == 'undefined'))
+			throw 'Not an APIs catalog  document';
+
 		// return the catalog document
 		return json;
-		
-	} catch (e) {
-		
-	}
-	
+
+	} catch (e) {}
+
 	return null;
 }
-
 
 /**
  * Scans the page and tries to detect if it contains a compatible content type.
  */
-function detectContentType() {
-	
+function detectContentType () {
 	try {
-		
+
+		// only allow the following
+		// content types:
+		// - appplication/json
+		// - text/yaml
+		var contentType = document.contentType;
+
+		if ((contentType !== 'application/json') &&
+			(contentType !== 'text/yaml')) {
+			return;
+		}
+
 		// get the document body
 		var body = document.body;
-		
+
 		// get the outer text of the page body
 		var content = body.textContent;
-		
+
 		/*
 		 * First check for Swagger content
 		 */
 		var data = checkIfSwaggerContent(content);
-		
+
 		/*
 		 * If the page contains a Swagger API document, try to parse it
 		 */
 		if (data)
-			return parseSwaggerDefinition(document.URL);
-		
+			return parseSwaggerDefinition(document.URL, data);
+
 		/*
 		 * Next check for APIs.json content
 		 */
 		data = checkIfApisCatalogContent(content);
-		
+
 		if (data) {
-			
 			var msg = {
-				name : 'onApiCatalogDefinition',
-				catalog : data
+				name: 'onApiCatalogDefinition',
+				catalog: data
 			};
-			
+
 			/*
 			 * broadcast the message
 			 */
 			chrome.runtime.sendMessage(undefined, msg, undefined);
 		}
-		
+
 	} catch (e) {
 		console.error(e);
 	}
-	
 }
 
 // The background page is asking us to find an address on the page.
 if (window == top) {
-	
+
 	/*
 	 * detect the content type of the page
 	 */
 	detectContentType();
-	
+
 }
--- chrome-ext/swagger-parser.js
+++ chrome-ext/swagger-parser.js
@@ -1,15 +1,15 @@
 /**
  * Swagger definition parser.
- * 
+ *
  * @author Chris Spiliotopoulos
  */
 
-String.prototype.startsWith = function(prefix) {
+String.prototype.startsWith = function (prefix) {
 	return this.indexOf(prefix) === 0;
-}
+};
 
-String.prototype.endsWith = function(suffix) {
-	return this.match(suffix + "$") == suffix;
+String.prototype.endsWith = function (suffix) {
+	return this.match(suffix + '$') == suffix;
 };
 
 /*
@@ -21,147 +21,132 @@ swagger.ed = swagger.ed || {};
 /**
  * APIs Catalog
  */
-swagger.ed.SwaggerParser = (function() {
-	
-	"use strict";
-	
+swagger.ed.SwaggerParser = (function () {
+	'use strict';
+
 	/*
 	 * PRIVATE METHODS
 	 */
 	var _private = {
-		
+
 		/**
 		 * Tries to parse a Swagger definition
 		 */
-		'parse' : function(url, cb) {
-			
+		'parse': function (url, data, cb) {
+			var opts = {
+				validateSchema: false,
+				resolve$Refs: false
+			};
+
 			/*
-			 * get the definition resource
+			 * try to parse the document using the Swagger 2.0 parser
 			 */
-			_private.getResourceAtUrl(url, function(err, content) {
-				
-				var opts = {
-					validateSchema : false,
-					resolve$Refs : false
-				};
-				
+			swagger.parser.parse(data, opts, function (err, api) {
+				if (!err)
+					return cb(null, api);
+
 				/*
-				 * try to parse the document using the Swagger 2.0 parser
+				 * try to convert the old Swagger doc into the new format
 				 */
-				swagger.parser.parse(url, opts, function(err, api) {
-					
-					if (!err)
+				_private.convertToNewSwaggerFormat(url, content, function (err, api) {
+					if (err)
+						return cb(err);
+					else
 						return cb(null, api);
-					
-					/*
-					 * try to convert the old Swagger doc into the new format
-					 */
-					_private.convertToNewSwaggerFormat(url, content, function(err, api) {
-						
-						if (err)
-							return cb(err);
-						else
-							return cb(null, api);
-					});
-					
 				});
-				
+
 			});
 		},
-		
+
 		/**
 		 * Tries to convert the spec to the latest version using the Swagger
 		 * converter.
 		 */
-		'convertToNewSwaggerFormat' : function(url, json, cb) {
-			
+		'convertToNewSwaggerFormat': function (url, json, cb) {
 			try {
-				
 				var paths = [];
-				
+
 				for ( var idx in json.apis) {
 					var path = url + json.apis[idx].path;
 					paths.push(path);
 				}
-				
+
 				var subresources = [];
-				
+
 				var api = {
-					resourceListing : json,
-					apiDeclarations : subresources
+					resourceListing: json,
+					apiDeclarations: subresources
 				};
-				
+
 				/*
 				 * fetch the sub-resources in parallel
 				 */
-				async.map(paths, function(path, cb) {
-					
+				async.map(paths, function (path, cb) {
+
 					// fetch the URL contents
-					_private.getResourceAtUrl(path, function(err, response) {
+					_private.getResourceAtUrl(path, function (err, response) {
 						if (err)
 							cb(err);
 						else
 							cb(null, response);
 					});
-					
-				}.bind(), function(err, results) {
-					
+
+				}.bind(), function (err, results) {
 					if (err)
 						return cb(err);
-					
+
 					/*
 					 * loop through the result set
 					 */
 					for ( var idx in results) {
-						
+
 						// parse the JSON contents
 						var res = results[idx];
 						subresources.push(res);
 					}
-					
+
 					// convert the API
 					var converted = SwaggerConverter.convert(api.resourceListing,
-							api.apiDeclarations);
-					
+						api.apiDeclarations);
+
 					// return the API
 					cb(null, converted);
 				});
-				
+
 			} catch (e) {
 				console.error(e);
 			}
-			
+
 		},
-		
+
 		/**
 		 * Sends an HTTP GET request to fetch the resource at the specified URL.
 		 */
-		'getResourceAtUrl' : function(url, cb) {
-			
+		'getResourceAtUrl': function (url, cb) {
+
 			// GET the resource
 			$.ajax({
-				url : url,
-				success : function(content, status) {
-					
+				url: url,
+				success: function (content, status) {
+
 					// return the content fetched
 					cb(null, content);
 				},
-				error : function(jqXHR, err) {
-					cb("Resource at [" + url + "] could not be fetched.");
+				error: function (jqXHR, err) {
+					cb('Resource at [' + url + '] could not be fetched.');
 				}
 			});
 		}
-	
+
 	};
-	
+
 	/*
 	 * PUBLIC METHODS
 	 */
 
 	return {
-		
-		parse : _private.parse
-	
-	}
+		parse: _private.parse
+
+	};
 
 })();
