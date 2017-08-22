2603cac9748fa15a5745c0ca04c3e04ed8cf5471
c.spiliotopoulos <c.spiliotopoulos@akazoo.com>
2017-08-22 13:36:31 +0300

    Split webpack production config into two modules for fixing ugligication issue

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,96 +2,116 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-let webpackConfig = {
+const webpackConfig = [
 
-  context: path.resolve(__dirname, 'src/'),
+  {
 
-  entry: {
-    base: './common/base.js',
-    openapis: './modules/openapis/index.js',
-    background: './browser/background/index.js',
-    content: './browser/content/index.js'
-  },
+    context: path.resolve(__dirname, 'src/'),
 
-  output: {
-    path: path.resolve(__dirname, 'extension/dist'),
-    filename: '[name].bundle.js'
-  },
+    entry: {
+      base: './common/base.js',
+      openapis: './modules/openapis/index.js',
+      background: './browser/background/index.js'
+    },
 
-  devtool: false,
+    output: {
+      path: path.resolve(__dirname, 'extension/dist'),
+      filename: '[name].bundle.js'
+    },
 
-  plugins: [
+    devtool: false,
 
-    new ExtractTextPlugin('styles.css'),
+    module: {
 
-    new webpack.ProvidePlugin({
-      jQuery: 'jquery',
-      $: 'jquery',
-      jquery: 'jquery',
-      Handlebars: 'handlebars/runtime'
-    }),
+      rules: [{
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.hbs$/,
+        use: [{
+          loader: 'handlebars-loader',
+          options: {
+            rootRelative: path.join(__dirname, 'extension', 'templates/'),
+            knownHelpers: [
+              'eq'
+            ],
+            knownHelpersOnly: false
+          }
+        }]
+      }
+      ]
+    },
 
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
-    //
-    // new webpack.optimize.UglifyJsPlugin({
-    //   parallel: true,
-    //   sourceMap: false,
-    //   exclude: /\/content.bundle.js/
-    // })
+    plugins: [
 
-  ]
+      new ExtractTextPlugin('styles.css'),
 
-};
+      new webpack.ProvidePlugin({
+        jQuery: 'jquery',
+        $: 'jquery',
+        jquery: 'jquery',
+        Handlebars: 'handlebars/runtime'
+      }),
 
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
 
-/*
- * CSS bundles
- */
-
-webpackConfig = merge(webpackConfig, {
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        use: 'css-loader'
+      new UglifyJSPlugin({
+        parallel: true,
+        sourceMap: false
       })
-    },
-    {
-      test: /\.(png|svg|jpg|gif)$/,
-      use: [
-        'file-loader'
-      ]
-    },
-    {
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: [
-        'file-loader'
-      ]
-    },
-    {
-      test: /\.html$/,
-      loader: 'html-loader'
-    },
-    {
-      test: /\.hbs$/,
-      use: [{
-        loader: 'handlebars-loader',
-        options: {
-          rootRelative: path.join(__dirname, 'extension', 'templates/'),
-          knownHelpers: [
-            'eq'
-          ],
-          knownHelpersOnly: false
-        }
-      }]
-    }
     ]
+
+  },
+
+  {
+
+    context: path.resolve(__dirname, 'src/'),
+
+    entry: {
+      content: './browser/content/index.js'
+    },
+
+    output: {
+      path: path.resolve(__dirname, 'extension/dist'),
+      filename: '[name].bundle.js'
+    },
+
+    devtool: false,
+
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      })
+    ]
+
   }
-});
+
+];
 
 
 module.exports = webpackConfig;
