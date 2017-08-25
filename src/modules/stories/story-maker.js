e226df26a4783b2b4a61bac38e81c1c6e0be4ebc
c.spiliotopoulos <c.spiliotopoulos@akazoo.com>
2017-08-25 21:39:21 +0300

    Updates

--- src/modules/stories/story-maker.js
+++ src/modules/stories/story-maker.js
@@ -16,10 +16,6 @@ export default (function() {
   /*
    * Private
    */
-  const steps = {
-    outline: tplStepOutline,
-    input: tplStepInput
-  };
 
   // the API definition instance
   let _api = null;
@@ -56,7 +52,7 @@ export default (function() {
       duration: 100,
       onVisible: () => {
         // select the first step
-        _onStepSelected('outline');
+        _onStepSelected('input');
 
         $modal.modal('refresh');
       },
@@ -87,12 +83,12 @@ export default (function() {
 
     try {
 
-      // validate the existing form
+      // validate the previous step
       if (_stepId) {
         _validateStep(_stepId);
       }
 
-      // remember the current step Id
+      // change step
       _stepId = stepId;
 
       const $step = $(`.modal .step[data-form="${stepId}"]`);
@@ -100,42 +96,110 @@ export default (function() {
       $('.modal .steps .step').removeClass('active');
       $step.addClass('active');
 
-      // get the selected form template
-      const tpl = steps[stepId];
+      // manage the step
+      if (_stepId === 'outline') {
+        _onOutlineStep();
+      } else if (_stepId === 'input') {
+        _onInputStep();
+      }
 
-      // create the model
-      const model = {
-        title: _api.title,
-        operations: _api.operationsBySummary
-      };
-
-      // render the form template
-      const $cnt = $('.modal #step-contents');
-      const html = tpl(model);
-      $cnt.html(html);
-
-      // bind the listeners
-      _bindFormListeners();
-
-      // bind validators
-      _bindFormValidators();
-
-      // populate the step with data
-      _populateStepWithData(stepId);
 
     } catch (e) {
       // silent
     }
   };
 
-  /**
-   * Binds all form listeners.
-   * @param  {[type]} $cnt [description]
-   * @return {[type]}      [description]
-   */
-  const _bindFormListeners = function() {
 
-    // outline form
+  /**
+   * Gathers the data entered in
+   * all steps.
+   * @return {[type]} [description]
+   */
+  const _validateStep = function(stepId) {
+
+    // step: outline
+    if (_stepId === 'outline') {
+
+      const $form = $(`form[data-form="${stepId}"]`);
+      $form.form('validate form');
+      const res = $form.form('is valid');
+
+      $modal.modal('refresh');
+
+      if ((!res) ||
+        ((typeof res !== 'boolean') && (!res[res.length - 1]))) {
+        throw new Error('Invalid step input');
+      }
+    }
+
+
+  };
+
+
+  /**
+   * Manage the outline step
+   * @return {[type]} [description]
+   */
+  const _onOutlineStep = function() {
+
+    // create the model
+    const model = {
+      title: _api.title,
+      operations: _api.operationsBySummary
+    };
+
+    // render the form template
+    const $cnt = $('.modal #step-contents');
+    const html = tplStepOutline(model);
+    $cnt.html(html);
+
+    /*
+     * populate with existing data
+     */
+
+    // title
+    $('[name="title"]', 'form[data-form="outline"]').val(_story.definition.title);
+
+    // description
+    $('[name="description"]', 'form[data-form="outline"]').val(_story.definition.description);
+
+    // operation
+    if (typeof _story.parts[0] !== 'undefined') {
+      $('[name="operation"]', 'form[data-form="outline"]').dropdown('set selected', _story.parts[0].operationId);
+    }
+
+    // form validators
+    $('.modal form[data-form="outline"]')
+      .form({
+        inline: true,
+        on: 'blur',
+        fields: {
+          title: {
+            identifier: 'title',
+            rules: [{
+              type: 'empty',
+              prompt: 'Please enter a story title'
+            }]
+          },
+          description: {
+            identifier: 'description',
+            rules: [{
+              type: 'empty',
+              prompt: 'Please write a short description'
+            }]
+          },
+          operation: {
+            identifier: 'operation',
+            rules: [{
+              type: 'empty',
+              prompt: 'Please select a base operation'
+            }]
+          }
+        }
+      });
+
+
+    // form listeners
     $('[name="title"]', 'form[data-form="outline"]')
       .on('change', (e) => {
         // set the title
@@ -165,92 +229,163 @@ export default (function() {
       });
   };
 
+
   /**
-   * Binds the form validators
+   * Manage the input step
    * @return {[type]} [description]
    */
-  const _bindFormValidators = function() {
+  const _onInputStep = function() {
 
-    // outline form
-    $('.modal form[data-form="outline"]')
-      .form({
-        inline: true,
-        on: 'blur',
-        fields: {
-          title: {
-            identifier: 'title',
-            rules: [{
-              type: 'empty',
-              prompt: 'Please enter a story title'
-            }]
-          },
-          description: {
-            identifier: 'description',
-            rules: [{
-              type: 'empty',
-              prompt: 'Please write a short description'
-            }]
-          },
-          operation: {
-            identifier: 'operation',
-            rules: [{
-              type: 'empty',
-              prompt: 'Please select a base operation'
-            }]
-          }
+    // get the selected operation
+    const opId = 'findPetsByStatus'; // _story.parts[0].operationId;
+
+    // get the operation definition
+    const operation = _.cloneDeep(_api.getOperation(opId));
+
+    _.each(operation.parameters, (o) => {
+      // check if the param has a schema
+      if (typeof o.schema !== 'undefined') {
+        // enrich the model with the object type
+        const type = o.schema.$$ref.replace('#/definitions/', '');
+        o.type = type;
+      }
+
+      // decide on the field type
+      if (o.type === 'array') {
+
+        // this should be a dropdown
+        o.fieldType = 'select';
+        o.fieldValues = o.items.enum;
+        o.defaultValue = o.items.default;
+
+        if (o.collectionFormat === 'multi') {
+          o.selectMultiple = true;
         }
-      });
+      } else if (o.type === 'boolean') {
+        o.fieldType = 'select';
+        o.fieldValues = ['true', 'false'];
+      } else if (o.type === 'file') {
+
+        console.log('File is not supported yet');
+      } else {
+
+        // otherwise use an input
+        o.fieldType = 'input';
+      }
+
+    });
+
+    // create the model
+    const model = {
+      title: _api.title,
+      parameters: operation.parameters
+    };
+
+    console.log(operation.parameters);
+
+    // render the form template
+    const $cnt = $('.modal #step-contents');
+    const html = tplStepInput(model);
+    $cnt.html(html);
+
+    // initialize dropdowns
+    $('.ui.dropdown', $cnt).dropdown();
+
+    // bind the dataset validators
+    _bindDatasetValidators(operation.parameters);
   };
 
 
   /**
-   * Gathers the data entered in
-   * all steps.
-   * @return {[type]} [description]
+   * Binds form validators for
+   * the input dataset.
+   * @param  {[type]} parameters [description]
+   * @return {[type]}            [description]
    */
-  const _validateStep = function(stepId) {
+  const _bindDatasetValidators = function(parameters) {
 
-    // step: outline
-    if (stepId === 'outline') {
+    const opts = {
+      inline: true,
+      on: 'blur',
+      fields: {}
+    };
 
-      const $form = $(`form[data-form="${stepId}"]`);
-      $form.form('validate form');
-      const res = $form.form('is valid');
+    // iterate through all params
+    _.each(parameters, (o) => {
 
-      $modal.modal('refresh');
+      // locate the field on the form
+      const name = o.name;
 
-      if ((!res) ||
-        ((typeof res !== 'boolean') && (!res[res.length-1]))) {
-        throw new Error('Invalid step input');
+      // get the field instance
+      const $field = $(`.modal .parameter[name="${name}"]`);
+
+      // add an entry to the map
+      const entry = {
+        identifier: name,
+        rules: []
+      };
+      opts.fields[name] = entry;
+
+      // add the rules
+      console.log(o);
+
+      // required
+      if (o.required) {
+        entry.rules.push({
+          type: 'empty',
+          prompt: 'This field is required'
+        });
       }
-    }
 
-
-  };
-
-
-  /**
-   * Populates the given step form
-   * with already stored data.
-   * @param  {[type]} stepId [description]
-   * @return {[type]}        [description]
-   */
-  const _populateStepWithData = function(stepId) {
-
-    if (stepId === 'outline') {
-      // title
-      $('[name="title"]', 'form[data-form="outline"]').val(_story.definition.title);
-
-      // description
-      $('[name="description"]', 'form[data-form="outline"]').val(_story.definition.description);
-
-      // operation
-      console.log(_story.parts[0].operationId);
-      if (typeof _story.parts[0] !== 'undefined') {
-        $('[name="operation"]', 'form[data-form="outline"]').dropdown('set selected', _story.parts[0].operationId);
+      switch (o.format) {
+        case 'integer':
+          entry.rules.push({
+            type: 'integer',
+            prompt: 'Value should be an integer'
+          });
+          break;
+        case 'float':
+        case 'double':
+          entry.rules.push({
+            type: 'decimal',
+            prompt: 'Value should be a decimal'
+          });
+          break;
+        default:
+          break;
       }
-    }
 
+      // min length
+      if (typeof o.minLength !== 'undefined') {
+        entry.rules.push({
+          type: `minLength[${o.minLength}]`,
+          prompt: `Value length should be >= ${o.minLength} characters`
+        });
+      }
+
+      // max length
+      if (typeof o.maxLength !== 'undefined') {
+        entry.rules.push({
+          type: `maxLength[${o.maxLength}]`,
+          prompt: `Value length should be <= ${o.maxLength} characters`
+        });
+      }
+
+      // pattern
+      if (typeof o.pattern !== 'undefined') {
+        entry.rules.push({
+          type: 'regExp',
+          value: o.pattern,
+          prompt: `Value should conform to pattern ${o.pattern}`
+        });
+      }
+
+    });
+
+    console.log($('.modal .form'));
+
+    // bind the validators
+    $('.modal .form').form(opts);
 
   };
 
